b75bcabfa7e265b4947891465ddedcbd3d62cc06
Christian <chrisdaq@gmail.com>
2024-11-13 10:32:56 -0400

    fix and run db seeders

--- database/seeders/AddPermissionsAndRoles.php
+++ database/seeders/AddPermissionsAndRoles.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class AddPermissionsAndRoles extends Seeder
 {
