9f21201c34946f9e41b22f74aeeec3ac319d472e
Christian <chrisdaq@gmail.com>
2024-11-14 15:06:49 -0400

    add adminlte documentation link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <a href="https://github.com/jeroennoten/Laravel-AdminLTE">admin repo</a>
+
                 </div>
             </div>
         </div>
