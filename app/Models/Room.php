a245305e63c171e2e1639e19fd86d75f04b6cfe3
Christian <chrisdaq@gmail.com>
2024-12-22 14:46:14 -0400

    add required model changes

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -18,7 +18,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Room extends Model
 {
-    
+
     protected $perPage = 20;
 
     /**
@@ -28,5 +28,8 @@ class Room extends Model
      */
     protected $fillable = ['name', 'description'];
 
-
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
